cab64796577985d3801601387e036cd19caae99e
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-02-06 18:26:44 +0100

    Added theme object with 4 themes palette

--- clockscript.js
+++ clockscript.js
@@ -46,3 +46,40 @@ setTimeout(() => {
     getTheme(cycleHour);
 }, (60 - cycleDate.getSeconds()) * 1000);
 
+
+const themes = {
+    morning: {
+        containerBackground : '#ffe9a6',
+        containerColor : '#4a4a4a',
+        clockBorder : '#ffcd80',
+        clockBoxShadow : 'rgba(212, 175, 55, 0.5)',
+        clockBackground : 'rgba(255, 255, 255, 0.8)',
+        clockTextShadow : '#ffdb00',
+    },
+    afternoon: {
+        containerBackground : '#aceaff',
+        containerColor : '#cdb500',
+        clockBorder : '#70bee2',
+        clockBoxShadow : 'rgba(55, 157, 212, 0.5)',
+        clockBackground : 'rgba(230, 254, 255, 0.8)',
+        clockTextShadow : '#94cdf8',
+    },
+    evening: {
+        containerBackground : '#475d85',
+        containerColor : '#d67b6b',
+        clockBorder : '#b0d5f5',
+        clockBoxShadow : 'rgba(212, 175, 55, 0.5)',
+        clockBackground : 'rgba(255, 206, 99, 0.8)',
+        clockTextShadow : '#ffdb85',
+    },
+    night: {
+        containerBackground : '#131862',
+        containerColor : '#bea9de',
+        clockBorder : '#462e82',
+        clockBoxShadow : 'rgba(32, 46, 195, 0.6)',
+        clockBackground : 'rgba(46,68,130, 0.8)',
+        clockTextShadow : '#87889c',
+    }
+};
+
+
